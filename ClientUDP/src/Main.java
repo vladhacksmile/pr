97a20eb9565b281fe705e33a70512b498c84503a
vladhacksmile <54905627+vladhacksmile@users.noreply.github.com>
2021-04-22 17:55:24 +0300

    Client - Lab6
    UDP client, which works with UDP Server and check, send/read requests (use DatagramChannel with NIO)
    
    Mode - if connection closed - set timeout 3 seconds and resend last command to the UDP server

---
+++ ClientUDP/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        UDPSocketClient client = new UDPSocketClient();
+        client.createAndListenSocket();
+    }
+}
---
+++ ClientUDP/src/ResponseHandler.java
@@ -0,0 +1,58 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.StreamCorruptedException;
+import java.net.PortUnreachableException;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class ResponseHandler extends Thread {
+
+    @Override
+    public void run() {
+        while(UDPSocketClient.runnable) {
+            if (UDPSocketClient.channel.isOpen()) {
+                try {
+                    byte[] incomingData = new byte[65515];
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(incomingData);
+                    UDPSocketClient.channel.receive(byteBuffer);
+                    byteBuffer.flip();
+                    ByteArrayInputStream in = new ByteArrayInputStream(incomingData);
+                    ObjectInputStream is = new ObjectInputStream(in);
+                    Answer answer = (Answer) is.readObject();
+                    if(answer.getAnswer().get(0).equals("connected")) {
+                        UDPSocketClient.connected = true;
+                        System.out.println("Соединение с сервером установлено!");
+                        if(UDPSocketClient.lastCommand != null) {
+                            System.out.println("Клиент переотправил команду заново!");
+                            UDPSocketClient.send(UDPSocketClient.lastCommand);
+                        }
+                    } else {
+                        UDPSocketClient.lastCommand = null;
+                        answer.printArray();
+                    }
+                } catch (PortUnreachableException e) {
+                    System.out.print("Не удалось подключиться к серверу! Попытка подключиться к серверу через 3 секунды");
+                    try {
+                        Thread.sleep(1000);
+                        System.out.print(".");
+                        Thread.sleep(1000);
+                        System.out.print(".");
+                        Thread.sleep(1000);
+                        System.out.print(".\n");
+                        UDPSocketClient.connect();
+                    } catch (InterruptedException interruptedException) {
+                        interruptedException.printStackTrace();
+                    }
+                } catch (ClassNotFoundException e) {
+                    System.err.println("Не удалось определить класс, возможно что-то с сериализацией данных или недостаток размера буфера!");
+                    UDPSocketClient.runnable = false;
+                } catch (StreamCorruptedException e) {
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+   }
+}
